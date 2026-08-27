2d647c3d64277caa32415dc984dddea9aa0dd9cc
Your Name <oa_nasri@esi.dz>
2026-08-27 14:23:31 +0100

    fix repo

--- run.py
+++
@@ -1,5 +0,0 @@
-from fastorganize.project import analyze_project
-from fastorganize.reporter import print_report
-project_path = "C:/Users/HP/Desktop/Siraj/backend/app/routes"
-results = analyze_project(project_path)
-print_report(results)
--- tests/test_scanner.py
+++
@@ -1,5 +0,0 @@
-from fastorganize.scanner import scan_project
-project="C:/Users/HP/Desktop/Siraj/backend/app/models"
-files=scan_project(project) 
-for file in files:
-  print(file )
